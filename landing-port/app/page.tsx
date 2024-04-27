ddfecba26b1997cbdbc4d38f54946e979d886be8
mictlanhalley <97858954+mictlanhalley@users.noreply.github.com>
2024-04-26 22:18:56 -0600

    add : agregando public y animacion

--- landing-port/app/page.tsx
+++ landing-port/app/page.tsx
@@ -1,10 +1,11 @@
+import CoverParticles from "@/components/cover-particles";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
-        <p>Cover</p>
+        <CoverParticles />
         <p>Introduccion</p>
       </div>
     </main>
